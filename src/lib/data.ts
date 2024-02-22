b7e5faa8989da87396586b621146260e4beb81a1
Jose Lezama <me@joseglego.io>
2024-02-21 23:21:00 -0300

    core: define basic data in lib/data but have to be moved to Sanity

---
+++ src/lib/data.ts
@@ -0,0 +1,91 @@
+// ToDo: All the data should comes from Sanity
+export const links = [
+  { id: "linkedin", url: "https://www.linkedin.com/company/jsconf-chile/" },
+  { id: "instagram", url: "https://www.instagram.com/jsconf.cl/" },
+  { id: "twitter", url: "https://twitter.com/jsconfcl/" },
+];
+
+export const data = [
+  {
+    cols: "1",
+    cards: [
+      {
+        title: "JSConf",
+        text: [
+          "JSConf te invita a un mundo donde JavaScript da forma al futuro. Este evento es un crisol de ideas innovadoras, charlas de expertos y una comunidad apasionada por la tecnología. Experimenta la combinación de tecnologías avanzadas de JavaScript, conecta con líderes de la industria y sé parte de un movimiento que impulsa la tecnología hacia adelante.",
+        ],
+        url: "",
+        actionTitle: "",
+      },
+    ],
+  },
+  {
+    cols: "2",
+    cards: [
+      {
+        title: "Ser Sponsor",
+        text: [
+          "Asóciate con JSConf como patrocinador y posiciona tu marca en la vanguardia de la industria tecnológica. Esta oportunidad única te permite conectar con líderes de pensamiento, ganar una exposición significativa y contribuir al crecimiento de la comunidad tecnológica. Tu apoyo ayuda a fomentar la innovación y conectar con una audiencia diversa.",
+        ],
+        url: "mailto:sponsor@jsconf.cl",
+        actionTitle: "Ser Sponsor",
+      },
+      {
+        title: "Suscribirse",
+        text: [
+          "Suscríbete a JSConf y nunca te pierdas las últimas novedades en JavaScript y noticias de la comunidad tecnológica. Nuestras actualizaciones proporcionan información sobre eventos próximos, anuncios de oradores y las últimas tendencias en tecnología. Mantente conectado con una red global de profesionales y entusiastas por igual.",
+        ],
+        url: "https://auth.jschile.org/",
+        actionTitle: "Suscribirme",
+      },
+    ],
+  },
+  {
+    cols: "3",
+    cards: [
+      {
+        title: "JSConf 2023",
+        text: [
+          "Revisita el legado de JSConf 2023 a través de nuestro archivo detallado. Revive los momentos que definieron el evento: charlas innovadoras, avances tecnológicos e interacciones comunitarias. Esta retrospectiva es un tesoro de conocimiento, inspiración y testimonio del espíritu vibrante de la comunidad tecnológica.",
+        ],
+        url: "https://2023.jsconf.cl/",
+        actionTitle: "Ver JSConf 2023",
+      },
+      {
+        title: "Galería 2023",
+        text: [
+          "Entra en el viaje visual de JSConf 2023 con nuestra galería. Cada foto captura la esencia del evento, desde sesiones innovadoras hasta networking espontáneo. Experimenta el ambiente de colaboración, innovación y el espíritu de una comunidad unida por la pasión por JavaScript.",
+        ],
+        url: "https://gallery.jsconf.cl/",
+        actionTitle: "Ver la Galería",
+      },
+      {
+        title: "JavaScript Chile",
+        text: [
+          "JavaScript Chile es más que una comunidad; es un centro de innovación y colaboración. Únete para interactuar con talentos locales, participar en proyectos pioneros y contribuir a un ecosistema próspero. Sé parte del movimiento que está dando forma al futuro de la tecnología en Chile y más allá.",
+        ],
+        url: "https://jschile.org/",
+        actionTitle: "Saber más de JSChile",
+      },
+    ],
+  },
+];
+
+export const footer = [
+  {
+    id: 1,
+    name: "Codigo de Conducta",
+    url: "https://github.com/jsconfcl/code_of_conduct",
+  },
+  { id: 2, name: "Contacto", url: "mailto:contacto@jsconf.cl" },
+  {
+    id: 3,
+    name: "MIT Licencse  - JSConf Chile",
+    url: "https://github.com/jsconfcl/jsconf-landing",
+  },
+  {
+    id: 4,
+    name: "Con ❤️ - El equipo JSConf Chile",
+    url: "https://github.com/jsconfcl",
+  },
+];
