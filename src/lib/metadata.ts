ac624e340cc2f126daec8a653bac7714a98dd19b
Jose Lezama <me@joseglego.io>
2024-02-25 21:26:51 -0300

    core: include metada & og-image

---
+++ src/lib/metadata.ts
@@ -0,0 +1,55 @@
+export const getMetaData = () => {
+  const url = "https://jsconf.cl/";
+  const title =
+    "JSConf Chile 2024 | Conferencia de Javascript en Santiago Chile";
+  const description =
+    "JSConf Chile 2024 - Muy pronto | Ya estamos trabajando en la nueva JSConf Chile. Mantente informado.";
+  const images = ["/images/og-image.jpg"];
+
+  return {
+    metadataBase: new URL(url),
+    title,
+    description,
+
+    keywords: [
+      "TechTon",
+      "JSChile",
+      "Comunidades",
+      "Streaming",
+      "Donación",
+      "Bomberos de Chile",
+    ],
+    authors: [{ name: "JSChile", url }],
+
+    // OpenGraph
+    openGraph: {
+      type: "website",
+      url,
+      title,
+      description,
+      images,
+    },
+
+    // Twitter
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images,
+    },
+  };
+};
+
+export const getViewports = () => {
+  return {
+    themeColor: [
+      { media: "(prefers-color-scheme: light)", color: "#F0E040" },
+      { media: "(prefers-color-scheme: dark)", color: "#1E2019" },
+    ],
+    viewport: {
+      width: "device-width",
+      initialScale: 1,
+      maximumScale: 1,
+    },
+  };
+};
