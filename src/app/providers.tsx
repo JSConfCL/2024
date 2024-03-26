50a0b77ac0c44ed8530313415edeed7738d33a3e
Jose Lezama <me@joseglego.io>
2024-03-26 01:30:31 -0300

    chore: define posthog provider

---
+++ src/app/providers.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
+
+if (
+  typeof window !== "undefined" &&
+  process.env.NEXT_PUBLIC_POSTHOG_KEY &&
+  process.env.NEXT_PUBLIC_POSTHOG_HOST
+) {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+  });
+}
+export function CSPostHogProvider({
+  children,
+}: {
+  children: JSX.Element | JSX.Element[];
+}) {
+  return <PostHogProvider client={posthog}>{children}</PostHogProvider>;
+}
