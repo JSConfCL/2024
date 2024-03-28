c1beb0d6e5a464a4acd2318add6f50d237f1e367
Irwing Naranjo <khalisser@gmail.com>
2024-03-28 13:48:42 -0300

    refactor: add dateToGoogleCalendarFormat function

--- src/lib/date.ts
+++ src/lib/date.ts
@@ -23,3 +23,11 @@ export const formatDate = (date: Date | null): string | null => {
 
   return `${months[monthIndex]} ${day.toString().padStart(2, "0")} ${year}`;
 };
+
+export const dateToGoogleCalendarFormat = (date: Date): string => {
+  const year = date.getFullYear();
+  const month = (date.getMonth() + 1).toString().padStart(2, '0');
+  const day = date.getDate().toString().padStart(2, '0');
+  
+  return [year,month,day].join('');
+}
