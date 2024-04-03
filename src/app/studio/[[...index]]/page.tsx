df721c285e00e499a34e7e31d91e8aa258153563
Irwing Naranjo <khalisser@gmail.com>
2024-04-02 22:01:34 -0300

    fix: import formatting in page.tsx and sanity.ts

--- src/app/studio/[[...index]]/page.tsx
+++ src/app/studio/[[...index]]/page.tsx
@@ -1,8 +1,9 @@
-'use client'
+"use client";
 
-import { NextStudio } from 'next-sanity/studio'
-import config from '../../../../sanity.config'
+import { NextStudio } from "next-sanity/studio";
+
+import config from "../../../../sanity.config";
 
 export default function StudioPage() {
-  return <NextStudio config={config} />
+  return <NextStudio config={config} />;
 }
