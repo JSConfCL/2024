24a6e3a22e61536c0aaa978a2e548a99939efc7e
Irwing Naranjo <khalisser@gmail.com>
2024-04-02 22:01:34 -0300

    fix: import formatting in page.tsx and sanity.ts

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -1,6 +1,6 @@
-import { SanityDocument } from "next-sanity";
 import { client } from "@/sanity/lib/client";
+import { SanityDocument } from "next-sanity";
 
-export async function sanityQuery (query: string) {
+export async function sanityQuery(query: string) {
   return await client.fetch<SanityDocument[]>(query);
 }
