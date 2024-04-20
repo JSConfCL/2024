27979180c13a5be54a0a962c30010999dc26863c
Irwing Naranjo <khalisser@gmail.com>
2024-04-20 12:20:25 -0400

    feat: add section component v1

---
+++ src/components/Section/Sections.tsx
@@ -0,0 +1,33 @@
+interface SectionProps extends React.HTMLProps<HTMLDivElement> {
+  colsSm: number;
+  colsMd: number;
+  colsLg: number;
+  gap: number;
+}
+
+function validateNumberCols(cols: number) {
+  return (cols === undefined || cols < 1) ? 1 : cols;
+}
+
+export const Section: React.FC<SectionProps> = ({ children, colsSm, colsMd, colsLg, gap, ...props }) => {
+
+  colsSm = validateNumberCols(colsSm);
+  colsMd = validateNumberCols(colsMd);
+  colsLg = validateNumberCols(colsLg);
+
+  const STYLES = `
+    w-full
+    gap-${gap}
+    grid
+    grid-cols-1
+    sm:grid-cols-${colsSm}
+    md:grid-cols-${colsMd}
+    lg:grid-cols-${colsLg}
+  `;
+
+  return (
+    <section {...props} className={props.className + STYLES}>
+      {children}
+    </section>
+  );
+};
