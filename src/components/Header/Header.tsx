4f14410a89bce558445ef118d464cb179078d3bd
Jose Lezama <me@joseglego.io>
2024-06-10 23:55:48 -0400

    fix(Header): include social links on NOHeader pages

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,6 +5,8 @@ import { usePathname } from "next/navigation";
 import type { Link as LinkType } from "@/api/types";
 
 import { Logo } from "@/components/Icons/Logo";
+import { SocialLink } from "@/components/SocialLink/SocialLink";
+import { links as socialLinks } from "@/lib/data";
 import { theme } from "@/lib/theme";
 import { cn } from "@/lib/utils";
 
@@ -18,24 +20,22 @@ interface HeaderProps {
 export function Header({ links }: HeaderProps) {
   const pathname = usePathname();
   return (
-    <header className="container relative z-10 mx-auto flex h-20 max-w-[1136px] items-center justify-between p-4">
+    <header className="container relative z-10 mx-auto flex h-20 max-w-[1136px] items-center justify-between p-4 text-jsconf-yellow">
       <Link href="/">
         <Logo color={theme?.colors?.jsconfYellow} size="36" />
       </Link>
       <div
         className={cn("hidden items-center md:flex", links ? "gap-2" : "gap-4")}
       >
-        {links &&
-          links.map(
-            (link) =>
-              link && (
-                <HeaderLink
-                  key={link.url}
-                  isActive={pathname == link.url}
-                  {...link}
-                />
-              ),
-          )}
+        {links
+          ? links.map((link) => (
+              <HeaderLink
+                key={link.url}
+                isActive={pathname == link.url}
+                {...link}
+              />
+            ))
+          : socialLinks.map((link) => <SocialLink key={link.id} link={link} />)}
       </div>
       {links?.length ? <MobileNav links={links} activePath={pathname} /> : null}
     </header>
